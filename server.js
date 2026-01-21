056d4b53535e8dafb6b7c7b48969a777252d69df
AbhinavKV369 <abhinavkvmayyil@gmail.com>
2026-01-21 21:54:24 +0530

    Created hosting environment

--- server.js
+++ server.js
@@ -3,35 +3,39 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import connectDB from "./config/connectDB.js";
-import authRoutes  from "./routes/authRoutes.js"
-import userRoutes from "./routes/userRoutes.js" 
-import taskRoutes from "./routes/taskRoutes.js"
-import User from "./models/User.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin:"http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+ origin: process.env.CLIENT_URL || "*",
+  credentials: true,
+}));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API Running...");
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (error) {
+    console.error("DB Error:", error);
+    res.status(500).json({ error: "Database connection failed" });
+  }
 });
 
-app.use("/api/auth" ,authRoutes );
-app.use("/api/users",userRoutes);
-app.use("/api/tasks",taskRoutes);
+app.get("/", (req, res) => {
+  res.send("API Running on Vercel 🚀");
+});
 
-app.use(errorHandler)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.use(errorHandler);
 
 export default app;
