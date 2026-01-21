1193376fb0f7c84be11801252a370bbed2ca557b
AbhinavKV369 <abhinavkvmayyil@gmail.com>
2026-01-21 21:56:20 +0530

    Fixed database

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,22 +1,13 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 const connectDB = async () => {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      bufferCommands: false,
-    });
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 };
 
-export default connectDB;
+export default connectDB;  
